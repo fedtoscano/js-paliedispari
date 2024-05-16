ffccf1b09c6488bbb4e40d9b5aafaf76a3dfea30
fedtoscano <federicotoscano611@gmail.com>
2024-05-16 15:31:40 +0200

    fix del ciclo while a riga 31

--- script/script.js
+++ script/script.js
@@ -6,8 +6,14 @@ function isPalyndrome(word){
     return word===invertedWord
 }
 
+
+
+// ! ****************************************************************************************
+
+
+
 let userChoiche = prompt("Scegli se giocare pari o dispari! (inserisci 'pari' o 'dispari')")
-    while(userChoiche!=="pari" && userChoiche!=="dispari"){//una delle due condizioni deve essere vera
+    while(userChoiche==="pari" && userChoiche ==="dispari"){//entrambe le condizioni sono valide per entrare nel loop
         userChoiche = prompt("Scegli se giocare pari o dispari! (inserisci 'pari' o 'dispari')")
     }
 
@@ -22,21 +28,30 @@ function makeRandomNum(){
     return Math.floor((Math.random()*5) +1);
 }
 let userNumber = Number.parseInt(prompt("Inserisci un numero da 1 a 5"),10)
-    while(userNumber<1 && userNumber>5){
+    while(userNumber<1 || userNumber>5){//una sola condizione è valida per entrare nel loop
         userNumber = Number.parseInt(prompt("Inserisci un numero da 1 a 5. Inserisci un numero valido!"),10)
     }
 
 const pcNumber = makeRandomNum()
-pariODispari(userNumber, pcNumber, pariDispari);
 
 function pariODispari(num1, num2, condizione){
     const somma = num1+num2
-        if((somma%2===0 && condizione===true) && (somma%2===1 && condizione===false)){
-            
-            console.log("Hai vinto!")
-            console.log(`Hai giocato ${num1}. Il pc ha giocato ${num2}`)
-        }else{
-            console.log("Hai perso!")
-            console.log(`Hai giocato ${num1}. Il pc ha giocato ${num2}`)
-        }
+    let scelta 
+    if(condizione){
+        scelta = "pari"
+    }else{
+        scelta = "dispari"
+    }
+    
+    if((somma%2===0 && scelta==="pari") && (somma%2===1 && scelta==="dispari")){
+        console.log("Hai vinto!")
+        console.log(`Hai giocato ${num1} e hai scelto ${scelta}. Il pc ha giocato ${num2}`)
+        console.log(`La somma è ${somma}`)
+    }else{
+        console.log("Hai perso!")
+        console.log(`Hai giocato ${num1} e hai scelto ${scelta}. Il pc ha giocato ${num2}`)
+        console.log(`La somma è ${somma}`)
+    }
 }
+
+pariODispari(userNumber, pcNumber, pariDispari);
